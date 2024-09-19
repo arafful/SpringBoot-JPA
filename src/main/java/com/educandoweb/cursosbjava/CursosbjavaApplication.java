8eefd4532074dcff7b09f329651a512575a23869
Andre Rafful <andre.rafful@gmail.com>
2024-09-19 12:10:52 -0300

    Projeto criado

---
+++ src/main/java/com/educandoweb/cursosbjava/CursosbjavaApplication.java
@@ -0,0 +1,14 @@
+package com.educandoweb.cursosbjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursosbjavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursosbjavaApplication.class, args);
+	}
+
+	
+}
---
+++ src/test/java/com/educandoweb/cursosbjava/CursosbjavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educandoweb.cursosbjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursosbjavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
